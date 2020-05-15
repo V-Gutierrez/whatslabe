9f94de9da3bc6cb6882bbb68730c518f228612bf
Jonatan Machado <jonatanmachado35@gmail.com>
2020-05-15 15:05:10 -0300

    routes para as conversas

---
+++ src/pages/Caio/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Caio() {
+    return <div />;
+}
+
+export default Caio;
---
+++ src/pages/Paula/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Paula() {
+    return <div />;
+}
+
+export default Paula;
