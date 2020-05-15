6877295affa994ccea64db5a36000523fd42925d
Jonatan Machado <jonatanmachado35@gmail.com>
2020-05-15 14:47:29 -0300

    lista de mensagens e tela principal do whats

--- src/pages/Historico/index.js
+++ src/pages/Historico/index.js
@@ -1,35 +1,74 @@
 import React, { useState } from 'react';
 
+import { MdChat } from 'react-icons/md';
+import { IoMdMore } from 'react-icons/io';
+
+//assets
+import {
+    Title,
+    Avatar,
+    List,
+    Item,
+    EnvelopeItem,
+    ListConversation,
+    Conversation,
+    Text,
+    IconBig,
+    Status,
+    Icons,
+} from './style';
+
+import Caio from '../../assets/caio.png';
+import Paula from '../../assets/paula.jpg';
+import Soter from '../../assets/soter.jpg';
+import Whats from '../../assets/iconegrande.jpg';
+import Eu from '../../assets/eulirico.jpg';
+
 function Historico(props) {
     const [conversations, setConversations] = useState([
         {
             id: 1,
             nome: 'Caio',
             msg: 'Bora para aula, que hoje tem evidência!',
-            avatar: '',
+            avatar: Caio,
         },
         {
             id: 2,
             nome: 'Paula',
             msg: 'E esse HTML que não está semântico?',
-            avatar: '',
+            avatar: Paula,
         },
         {
             id: 3,
             nome: 'Soter',
             msg: 'Legal que tal agora fazer com bananinha?',
-            avatar: '',
+            avatar: Soter,
         },
     ]);
     return (
-        <ul>
-            {conversations.map((conversation) => (
-                <li key={conversation.id}>
-                    {conversation.nome} | {conversation.msg}
-                </li>
-            ))}
-        </ul>
+        <ListConversation>
+            <List>
+                <Status>
+                    <Avatar src={Eu} />
+                    <Icons>
+                        <MdChat size={24} color="#666" />
+                        <IoMdMore size={24} color="#666" />
+                    </Icons>
+                </Status>
+                {conversations.map((conversation) => (
+                    <Item key={conversation.id}>
+                        <EnvelopeItem>
+                            <Avatar src={conversation.avatar}></Avatar>
+                        </EnvelopeItem>
+                        <Title>{conversation.nome}</Title>
+                        <Text>{conversation.msg}</Text>
+                    </Item>
+                ))}
+            </List>
+            <Conversation>
+                <IconBig src={Whats} />
+            </Conversation>
+        </ListConversation>
     );
 }
-
 export default Historico;
--- src/pages/Historico/style.js
+++ src/pages/Historico/style.js
@@ -1,10 +1,85 @@
 import styled from 'styled-components';
 
+export const ListConversation = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+`;
+
 export const List = styled.div`
+    flex: 30%;
+`;
+
+export const Item = styled.div`
+    display: flex;
+    margin: 10px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    height: 72px;
+    width: 33vw;
+    pointer-events: all;
     position: relative;
 `;
 
 export const Title = styled.h1`
+    padding: 0px 15px 0 13px;
+    display: flex;
+    align-items: center;
     font-size: 20px;
     font-weight: bold;
+`;
+
+export const EnvelopeItem = styled.div`
+    padding: 10px 3px 0 13px;
+`;
+
+export const Avatar = styled.img`
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+    flex: none;
+    display: flex;
+    align-items: center;
+`;
+
+export const Conversation = styled.div`
+    flex: 70%;
+`;
+
+export const Text = styled.p`
+    border-bottom: 1px solid #000;
+`;
+
+export const IconBig = styled.img`
+    margin-top: 100px;
+    margin-left: 250px;
+    border-radius: 50%;
+    align-items: center;
+    flex: none;
+    display: flex;
+`;
+
+export const Status = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 90%;
+    height: 72px;
+    background-color: #e6e6e6;
+
+    img {
+        margin-top: 10px;
+        align-items: center;
+        margin-left: 23px;
+    }
+`;
+
+export const Icons = styled.span`
+    display: flex;
+    flex-direction: row;
+    align-self: center;
+    margin: 10px;
+    margin-left: 300px;
+    justify-content: space-between;
 `;
