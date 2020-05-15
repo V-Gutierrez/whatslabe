524fd8e6a0232a6d663c1851393cd9c8196b98a1
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-05-14 22:07:43 -0400

    Template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyle } from "./styledcomponents/GlobalStyles";
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <h1>Hello World!</h1>
+        </>
+    );
+}
+
+export default App;
